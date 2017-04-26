379d81b606a7e0485730158904778d3684175d20
Luigi Du <luigifloresdu@gmail.com>
2017-04-26 17:28:19 -0400

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*******
+ *   Luigi F. Du
+ *   luigifloresdu@gmail.com
+ *   EdX Harvard CS50
+ *   Week 1, Problem Set 0
+ *   Prints "Hello, world!"
+ *******
+ 
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, world!\n");
+    return 0;
+}
